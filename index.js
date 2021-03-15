f5f32d9c135ca5a24e6f4ff076e43241779ce721
close01 <75597371+close01@users.noreply.github.com>
2021-03-15 14:59:27 +0700

    line approve

--- index.js
+++ index.js
@@ -95,21 +95,6 @@ app.put('/api/edit/user/:doc', (req, res) => {
         res.json(edit);
 });
      
-//แก้ไข proflie hr
-// app.put('/api/edit/user/hr/:doc', (req, res) => {
-//     const edit = {
-//         name: req.body.name,
-//         nickname: req.body.nickname,
-//         position: req.body.position
-//     }
-//     firestore.collection("user").doc(req.params.doc).update({ 
-//         name: edit.name,
-//         nickname: edit.nickname,
-//         position: edit.position
-//     });
-//         res.json(edit);
-// });
-
 // เก็บใบลา
 app.post('/api/post/leave',async (req,res) => {
 
@@ -221,37 +206,31 @@ app.get('/api/get/approve',async (req, res) => {
     
 });
 //update status approve ส่งไอดีของใบลา
-app.put('/api/updateStatus/:doc', (req,res) => {
-    // let idUser = ""
-    // const LINE_MESSAGING_API_USER = 'https://api.line.me/v2/bot/message/push';
-    // const LINE_HEADER_USER = {
-    //     'Content-Type': 'application/json',
-    //     'Authorization': `Bearer {dCibnFNR1wHpjpqf51ArFk+4bsUShozYw3QIFr0U1r2adBk+/aNGSdm738J6qqGt5elkLO4eBwlTZz0jdD40+rAG42fLo9sD8Mhb4YLpxNDD80OLTeQlWo8FAvJxald9klaVQ5ei/a9aDKPcLavD5AdB04t89/1O/w1cDnyilFU=}`
-    //   };
+app.put('/api/updateStatus/:doc',async (req,res) => {
     const approve = {
         status: req.body.status
     }
-    firestore.collection("leave").doc(req.params.doc).update({
+    await firestore.collection("leave").doc(req.params.doc).update({
         status: approve.status
     })
-    // await firestore.collection("leave").doc(req.params.doc).get().then(function(docs){
-    //     idUser = docs.data().userId
-    //     console.log(idUser);
-    // })
-    // console.log(idUser);
-    // request({
-    //     method: `POST`,
-    //     uri: `${LINE_MESSAGING_API_USER}`,
-    //     headers: LINE_HEADER_USER,
-    //     body: JSON.stringify({
-    //       to: idUser,
-    //     // to: idHr,
-    //       messages: [{
-    //           type: "text",
-    //           text: "The status of your leave request is updated."
-    //       }]
-    //       })
-    //   });
+    await firestore.collection("leave").doc(req.params.doc).get().then(function(docs){
+        idUser = docs.data().userId
+        console.log(idUser);
+    })
+    console.log(idUser);
+    request({
+        method: `POST`,
+        uri: `${LINE_MESSAGING_API_USER}`,
+        headers: LINE_HEADER_USER,
+        body: JSON.stringify({
+          to: idUser,
+        // to: idHr,
+          messages: [{
+              type: "text",
+              text: "The status of your leave request is updated."
+          }]
+          })
+      });
     res.json(approve)
 })
 
