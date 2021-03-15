8df0662e15de5e28d617b77f9d11ea053d33a772
close01 <75597371+close01@users.noreply.github.com>
2021-03-15 15:11:26 +0700

    line approve

--- index.js
+++ index.js
@@ -207,23 +207,24 @@ app.get('/api/get/approve',async (req, res) => {
 });
 //update status approve ส่งไอดีของใบลา
 app.put('/api/updateStatus/:doc',async (req,res) => {
-    const approve = {
-        status: req.body.status
-    }
-    firestore.collection("leave").doc(req.params.doc).update({
-        status: approve.status
-    })
-    await firestore.collection("leave").doc(req.params.doc).get().then(function(docs){
-        idUser = docs.data().userId
-        console.log(idUser);
-    })
     let idUser = ""
-    console.log(idUser);
     const LINE_MESSAGING_API_USER = 'https://api.line.me/v2/bot/message/push';
     const LINE_HEADER_USER = {
         'Content-Type': 'application/json',
         'Authorization': `Bearer {dCibnFNR1wHpjpqf51ArFk+4bsUShozYw3QIFr0U1r2adBk+/aNGSdm738J6qqGt5elkLO4eBwlTZz0jdD40+rAG42fLo9sD8Mhb4YLpxNDD80OLTeQlWo8FAvJxald9klaVQ5ei/a9aDKPcLavD5AdB04t89/1O/w1cDnyilFU=}`
       };
+    const approve = {
+        status: req.body.status
+    }
+    await firestore.collection("leave").doc(req.params.doc).update({
+        status: approve.status
+    })
+    
+    await firestore.collection("leave").doc(req.params.doc).get().then(function(docs){
+        idUser = docs.data().userId
+        console.log(idUser);
+    })
+    console.log(idUser);
     request({
         method: `POST`,
         uri: `${LINE_MESSAGING_API_USER}`,
